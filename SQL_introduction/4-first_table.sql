79eb9aeed78f1cef31b45403c35a66da3691bcfd
Ayoubbahi99 <aelbahi1999@gmail.com>
2024-02-09 03:08:23 -0500

    task4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 --Create table if not Exists (first_table) with two columns id, name
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
